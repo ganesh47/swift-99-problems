aebf90780109c4be40de6970b07c6d9d8cb03699
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2022-10-28 00:59:38 +0530

    P10: Solution

---
+++ Sources/List/ListP10.swift
@@ -0,0 +1,46 @@
+//
+// P09: Pack consequitive elements, as sublists
+//
+
+import Foundation
+
+//Because a list has Equatable protocol implemented, it makes it easier to eliminate consecutive duplicates!
+
+public struct Encoded<T>: Equatable,CustomStringConvertible where T: Equatable {
+    public var description: String
+
+    public static func ==(lhs: Encoded<T>, rhs: Encoded<T>) -> Bool {
+        (lhs.item == rhs.item) && (lhs.count == rhs.count)
+    }
+
+    let count: Int
+    let item: T
+
+    init(_ count: Int, _ item: T) {
+        self.count = count
+        self.item = item
+        description="(\(count),\(item))"
+    }
+}
+
+public extension List {
+    func RLE<U>() -> List<U>? where U == Encoded<T>, T: Equatable {
+        var toBeReturnable = List<U>(Encoded(1, head))
+        if (tail == nil) {
+            return toBeReturnable
+        }
+        var prevElem = head
+        var register = 1
+        for item in tail! {
+            if prevElem != item {
+                toBeReturnable = toBeReturnable?.append(Encoded(register, prevElem))
+                register = 1
+            } else {
+                register = register + 1
+            }
+            prevElem = item
+        }
+        toBeReturnable = toBeReturnable?.append(Encoded(register, prevElem))
+        return toBeReturnable?.tail
+    }
+}
---
+++ Tests/ListTests/LIstP10Tests.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+@testable import List
+
+typealias IntPair = (Int, Int)
+
+
+final class ListP10Tests: XCTestCase {
+
+    func testP10RLE() {
+        self.measure {
+            XCTAssertEqual(List(1, 2)?.RLE(), List(Encoded(1, 1), Encoded(1, 2)), "Should've RLE'd")
+            XCTAssertEqual(List(1,1, 2)?.RLE(), List(Encoded(2, 1), Encoded(1, 2)), "Should've RLE'd")
+            XCTAssertEqual(List(1,1, 2,2,3,3,4,4,4,4,4)?.RLE(), List(Encoded(2, 1), Encoded(2, 2),Encoded(2,3),Encoded(5,4)), "Should've RLE'd")
+        }
+    }
+
+}
