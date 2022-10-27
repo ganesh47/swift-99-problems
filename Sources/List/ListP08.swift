24356f2dbd368dae7cbc5d739e0ad8bc534caa51
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2022-10-27 23:49:16 +0530

    P08: Solution

--- Sources/List/ListP08.swift
+++ Sources/List/ListP08.swift
@@ -6,19 +6,59 @@ import Foundation
 
 //Because a list has Equatable protocol implemented, it makes it easier to eliminate consecutive duplicates!
 
-
-public extension List {
-    func tailAt(_ at: Int, starting: Int = -1) -> List<T>? {
-        precondition(at >= 0, "Index should at-least be zero!")
-        return starting + 1 == at ? tail : tail?.tailAt(at, starting: starting + 1)
-    }
-
-    func deleteAt(_ index: Int) -> List<T> {
-        precondition(length() > 0, "Cannot empty a list")
-        if tailAt(index) != nil {
-            tail = tailAt(index)
+public struct ListIterator<T>: IteratorProtocol {
+    mutating public func next() -> T? {
+        if list != nil {
+            let element = list?.head
+            list = list?.tail
+            return element
         }
-        return self
+        return nil
     }
 
+    public typealias Element = T
+    var list: List<T>?
+
+    init(_ list: List<T>) {
+        self.list = list
+    }
+}
+
+extension List: Sequence {
+    public typealias Element = T
+    public typealias Iterator = ListIterator<T>
+
+    public func makeIterator() -> Iterator {
+        ListIterator<T>(self)
+    }
+
+    func deleteAt(_ index: Int) -> List<T>? {
+        var returningList = index == 0 ? tail : List(head)
+        if (index == 0 || tail == nil) {
+            return returningList
+        }
+        var i = 0
+        for item in tail! {
+            i = i + 1
+            if (i != index) {
+                returningList = returningList?.append(item)
+            }
+        }
+        return returningList
+    }
+
+    public func removeConsecutiveDuplicates() -> List<T>? where T: Equatable {
+        var returnableList = List(head)
+        if tail == nil {
+            return returnableList
+        }
+        var prevElem = head
+        for item in tail! {
+            if prevElem != item {
+                returnableList = returnableList?.append(item)
+            }
+            prevElem = item
+        }
+        return returnableList
+    }
 }
--- Tests/ListTests/LIstP08Tests.swift
+++ Tests/ListTests/LIstP08Tests.swift
@@ -7,9 +7,26 @@ import XCTest
 @testable import List
 
 final class ListP08Tests: XCTestCase {
-    func testP08Delete() throws {
-          XCTAssertEqual(List(1,2,3)?.deleteAt(1) , List(1,3), "Should remain unaltered")
+    func testP08Append() throws {
+        XCTAssertEqual(List(1)?.append(2), List(1, 2), "Should've appended 2 to the end!")
+        XCTAssertEqual(List(1, 3)?.append(2), List(1, 3, 2), "Should've appended 2 to the end!")
+        XCTAssertEqual(List(1, 3)?.append(2), List(1, 3, 2), "Should've appended 2 to the end!")
+    }
 
+    func testP08Delete() throws {
+        self.measure {
+            XCTAssertEqual(List(1, 2, 3)?.deleteAt(1), List(1, 3), "Should've removed mid-element")
+            XCTAssertEqual(List(1, 2, 3)?.deleteAt(2), List(1, 2), "Should've removed last element")
+            XCTAssertEqual(List(1, 2, 3)?.deleteAt(0), List(2, 3), "Should've removed head-element")
+        }
+    }
+
+    func testP08removeConsecutiveDuplicates() {
+        self.measure {
+            XCTAssertEqual(List(1, 2)?.removeConsecutiveDuplicates(), List(1, 2), "Should remain unchanged")
+            XCTAssertEqual(List(1, 2, 3)?.removeConsecutiveDuplicates(), List(1, 2, 3), "Should remain unchanged")
+            XCTAssertEqual(List(1, 2, 3, 3)?.removeConsecutiveDuplicates(), List(1, 2, 3), "Should've changed")
+        }
     }
 
 }
