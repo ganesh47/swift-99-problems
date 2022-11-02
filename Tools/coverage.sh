4938e3a2e15f64bc0ecdbd2a966e140680592140
graman47 <22994026+ganesh47@users.noreply.github.com>
2022-11-03 00:53:59 +0530

    Adding readme with compiler warnings

--- Tools/coverage.sh
+++ Tools/coverage.sh
@@ -8,6 +8,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 ${prefixCmd} llvm-cov show --format=html --instr-profile .build/debug/codecov/default.profdata ${objectArg} > code-coverage-report.html
 
+
+export COVERAGE_TEXT=`${prefixCmd} llvm-cov report --instr-profile .build/debug/codecov/default.profdata ${objectArg}`
+export COMPILER_WARNINGS=`swift build | grep warning`
 cat README.md.template | envsubst > README.md
-${prefixCmd} llvm-cov report --instr-profile .build/debug/codecov/default.profdata ${objectArg} >> README.md
-echo "</pre>" >> README.md
