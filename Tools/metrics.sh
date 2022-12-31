28121f87f8e97f62b444f75123f02305ccd66280
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2023-01-01 01:49:31 +0530

    Adding perf metrics capturer

--- Tools/metrics.sh
+++ Tools/metrics.sh
@@ -4,4 +4,4 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
   prefixCmd="--posix"
 fi
-swift test --enable-code-coverage | tee >(grep 'measured'| sed $prefixCmd -r s/\"/\'/g | sed -r "s/(.*): Test Case \'?\-?\[?(.*)\]? measured \[(.*)\] (.*)/{\"source\":\"\1\",\"test\":\"\2\", \"measures\": \"\3\", \"values\": \"\4\" }/g" >metrics.json)
+swift test --enable-code-coverage | tee >(grep 'measured'| sed -r s/\"/\'/g | sed $prefixCmd -r "s/(.*): Test Case \'?\-?\[?(.*)\]? measured \[(.*)\] (.*)/{\"source\":\"\1\",\"test\":\"\2\", \"measures\": \"\3\", \"values\": \"\4\" }/g" >metrics.json)
